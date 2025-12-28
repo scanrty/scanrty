8f3995eafa020bad3cf05ef4000ad03e2b1b511e
Anthony <info.client@scanrty.com>
2025-12-28 20:24:20 +0100

    Fix: Webpack externals pour Puppeteer + retrait output export

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // Retirer 'output: export' car on a besoin des API routes
   images: {
     unoptimized: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Exclure puppeteer-core et chromium du bundle webpack
+      config.externals = [...(config.externals || []), 'puppeteer-core', '@sparticuz/chromium']
+    }
+    return config
   },
 }
 
